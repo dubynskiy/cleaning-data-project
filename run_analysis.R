7981b512897822c7315f2ecb538800e892280ba6
dubynskiy <dubynskiy@physics.umn.edu>
2014-08-19 22:14:16 -0700

    tidy set

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,36 @@
 rm(list=ls())
 
-X_test = read.table('.\\test\\X_test.txt')
-Y_test = read.table('.\\test\\y_test.txt')
-s_test = read.table('.\\test\\subject_test.txt')   
+setwd('/Users/Zerg/Downloads/UCI-HAR-Dataset/')
 
-X_train = read.table('.\\train\\X_train.txt')
-Y_train = read.table('.\\train\\y_train.txt')
-s_train = read.table('.\\train\\subject_train.txt') 
+# X_test = read.table('.\\test\\X_test.txt')
+# Y_test = read.table('.\\test\\y_test.txt')
+# s_test = read.table('.\\test\\subject_test.txt')   
+# 
+# X_train = read.table('.\\train\\X_train.txt')
+# Y_train = read.table('.\\train\\y_train.txt')
+# s_train = read.table('.\\train\\subject_train.txt') 
+# 
+# X_total = rbind(X_test, X_train)
+# Y_total = rbind(Y_test, Y_train)
+# s_total = rbind(s_test, s_train)
+# 
+# features = read.table('.\\features.txt')  
+# activity = read.table('.\\activity_labels.txt') 
+
+X_test = read.table('./test/X_test.txt')
+Y_test = read.table('./test/y_test.txt')
+s_test = read.table('./test/subject_test.txt')   
+
+X_train = read.table('./train/X_train.txt')
+Y_train = read.table('./train/y_train.txt')
+s_train = read.table('./train/subject_train.txt') 
 
 X_total = rbind(X_test, X_train)
 Y_total = rbind(Y_test, Y_train)
 s_total = rbind(s_test, s_train)
 
-features = read.table('.\\features.txt')  
-activity = read.table('.\\activity_labels.txt') 
+features = read.table('./features.txt')  
+activity = read.table('./activity_labels.txt') 
 
 chars = as.character(features[,2])
 value = '-mean|-std'
@@ -24,7 +41,8 @@ features[idx,2]
 sum(idx)
 
 X_raw = X_total[,idx]
-Y_raw = sapply(Y_total, function(x) as.factor(activity[x,2]))
+Y_raw = sapply(Y_total, function(x) activity[x,2])
+Y_raw = data.frame(Y_raw)
 
 names(X_raw) = as.character(features[idx,2])
 names(Y_raw) = 'Activity'
@@ -32,4 +50,7 @@ names(s_total) = 'Subject'
 
 data = cbind(s_total, Y_raw, X_raw)
 
-tidy = aggregate( data[,-c(1,2)] ~ data$Subject+data$Activity, data[,-c(1,2)], mean )
+tidy1 = aggregate( . ~ Activity + Subject, data, mean )
+tidy2 = tidy1[c(2,1,3:81)] 
+
+tidy3 = aggregate( . ~ Subject + Activity, data, mean )
